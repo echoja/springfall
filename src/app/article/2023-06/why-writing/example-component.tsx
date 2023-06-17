ed0367d0ea44d392fdcd15e911c41a09d73dd0f7
Kim Tae Hoon <eszqsc112@gmail.com>
2023-06-17 15:51:43 +0900

    스타일 개선 및 중꺾그마 수정

---
+++ src/app/article/2023-06/why-writing/example-component.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+const ExampleComponent = () => (
+  <div className="flex items-center justify-center py-20 bg-orange-200 rounded-lg shadow-lg">
+    <motion.div
+      className="flex items-center justify-center w-48 h-24 bg-white rounded-full cursor-pointer select-none"
+      whileHover={{ scale: 1.2 }}
+      whileTap={{ scale: 0.9, backgroundColor: "#f6a767" }}
+      transition={{ type: "spring", stiffness: 400, damping: 17 }}
+    >
+      CLICK!
+    </motion.div>
+  </div>
+);
+
+export default ExampleComponent;
--- src/modules/article/ArticleHeader.tsx
+++ src/modules/article/ArticleHeader.tsx
@@ -5,15 +5,18 @@ import FaSolidAxe from "@modules/icons/FaSolidAxe";
 export default function ArticleHeader({
   title,
   updatedAt,
+  desc,
 }: {
   title: string;
   updatedAt: string;
+  desc?: string;
 }) {
   return (
     <>
       <header className="mb-5">
         <h1 className="text-3xl font-semibold">{title}</h1>
       </header>
+      <p>{desc}</p>
       <div className="inline-flex items-center gap-2 text-gray-500 mb-7">
         <FaSolidAxe className="w-4 h-4" />
         <span>{dayjs(updatedAt).format("YYYY.MM.DD.")}</span>
