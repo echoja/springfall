83108c58fb3754588e9c05081cb0a44e0b726674
Kim Tae Hoon <eszqsc112@gmail.com>
2023-06-24 16:14:36 +0900

    update

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,12 @@
 /// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@common": path.resolve(__dirname, "./src/common"),
