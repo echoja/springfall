8e18cd2be320f1717c7da05227f2bca1eff8c2f4
Kim Tae Hoon <eszqsc112@gmail.com>
2023-07-16 08:35:11 +0900

    draw-io 글 옮기기

--- src/modules/article/items.tsx
+++ src/modules/article/items.tsx
@@ -2,10 +2,17 @@ import dayjs from "dayjs";
 
 import { item as NodejsStream } from "@app/article/2021-12/node-js-stream/page.mdx";
 import { item as PussInBoots } from "@app/article/2023-02/puss-in-boots/page.mdx";
+import { item as DrawIOAutoLayout } from "@app/article/2023-03/draw-io-auto-layout/page.mdx";
 import { item as CompanyContest } from "@app/article/2023-06/company-contest/page.mdx";
 import { item as WhyWriting } from "@app/article/2023-06/why-writing/page.mdx";
 
-const items = [PussInBoots, CompanyContest, WhyWriting, NodejsStream];
+const items = [
+  PussInBoots,
+  CompanyContest,
+  WhyWriting,
+  NodejsStream,
+  DrawIOAutoLayout,
+];
 
 items.sort((a, b) => dayjs(b.createdAt).diff(a.createdAt));
 
