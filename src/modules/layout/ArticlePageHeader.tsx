cb2c8ba02f08e3e15c6b929a5a3177d721d48b2e
Kim Tae Hoon <eszqsc112@gmail.com>
2023-09-11 23:42:31 +0900

    fix props name

--- src/modules/layout/ArticlePageHeader.tsx
+++ src/modules/layout/ArticlePageHeader.tsx
@@ -20,9 +20,9 @@ const ArticlePageHeader = () => {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className="w-4 h-4 lucide lucide-list"
         >
           <line x1="8" x2="21" y1="6" y2="6" />
