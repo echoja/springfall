c13c4f35faa08ab14de82a578a88a44aa6c53a44
Kim Tae Hoon <eszqsc112@gmail.com>
2024-03-14 15:52:31 +0900

    install channeltalk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,18 @@ window.CodenButter("boot", { siteId: "otoyyikrrj", auto: true });
 `,
           }}
         ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+
+  (function(){var w=window;if(w.ChannelIO){return w.console.error("ChannelIO script included twice.");}var ch=function(){ch.c(arguments);};ch.q=[];ch.c=function(args){ch.q.push(args);};w.ChannelIO=ch;function l(){if(w.ChannelIOInitialized){return;}w.ChannelIOInitialized=true;var s=document.createElement("script");s.type="text/javascript";s.async=true;s.src="https://cdn.channel.io/plugin/ch-plugin-web.js";var x=document.getElementsByTagName("script")[0];if(x.parentNode){x.parentNode.insertBefore(s,x);}}if(document.readyState==="complete"){l();}else{w.addEventListener("DOMContentLoaded",l);w.addEventListener("load",l);}})();
+
+  ChannelIO('boot', {
+    "pluginKey": "8c19f1cf-ca25-474a-9a4f-6151030823f0"
+  });
+`,
+          }}
+        ></script>
       </head>
       <body>
         <Providers>
