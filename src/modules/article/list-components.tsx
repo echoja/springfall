57efe5a1c474db426eca2892ba70b3f1d99f0293
Kim Tae Hoon <eszqsc112@gmail.com>
2024-12-28 20:40:04 +0900

    #4 ul, li props forwarding 할 수 있도록 수정

--- src/modules/article/list-components.tsx
+++ src/modules/article/list-components.tsx
@@ -1,13 +1,32 @@
+import { twMerge } from "tailwind-merge";
 import style from "./style.module.css";
 
-export function OrderedList({ children }: { children?: React.ReactNode }) {
-  return <ol className={style.ol}>{children}</ol>;
+export function OrderedList({
+  children,
+  ...props
+}: React.ComponentProps<"ol">) {
+  return (
+    <ol {...props} className={twMerge(props.className, style.ol)}>
+      {children}
+    </ol>
+  );
 }
 
-export function UnorderdList({ children }: { children?: React.ReactNode }) {
-  return <ul className={style.ul}>{children}</ul>;
+export function UnorderdList({
+  children,
+  ...props
+}: React.ComponentProps<"ul">) {
+  return (
+    <ul {...props} className={twMerge(props.className, style.ul)}>
+      {children}
+    </ul>
+  );
 }
 
-export function ListItem({ children }: { children?: React.ReactNode }) {
-  return <li className={style.li}>{children}</li>;
+export function ListItem({ children, ...props }: React.ComponentProps<"li">) {
+  return (
+    <li {...props} className={twMerge(props.className, style.li)}>
+      {children}
+    </li>
+  );
 }
