f3d1d19ab47699026130068fe0d4dc0f0665853a
Taehoon Kim <eszqsc112@gmail.com>
2025-02-15 21:05:00 +0900

    글 보강 및 패키지 업그레이드

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,7 +13,6 @@ export default defineConfig({
       "@modules": path.resolve(__dirname, "./src/modules"),
     },
   },
-  // @ts-expect-error - Vite doesn't have a type for this yet
   test: {
     globals: true,
     environment: "happy-dom",
