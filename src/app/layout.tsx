740d437c01ac7b0014e74cb309daa1cec8961596
Kim Tae Hoon <eszqsc112@gmail.com>
2025-12-21 23:52:56 +0900

    CodenButter 스크립트 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,18 +55,6 @@ export default function RootLayout({
 `,
           }}
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-(function (co,de,n,but,t,e,r){!n[co]&&(n[co]=function(){
-(n[co].q=n[co].q||[]).push(arguments);});e=t.createElement(but);
-e.async=true;e.src=de;r=t.getElementsByTagName(but)[0];
-r.parentNode.insertBefore(e, r);
-})("CodenButter", "https://buttr.dev/butter.js", window, "script", document);
-window.CodenButter("boot", { siteId: "otoyyikrrj", auto: true });
-`,
-          }}
-        ></script>
         {/* GTM */}
         {process.env.NEXT_PUBLIC_GTM_ID ? (
           <script
