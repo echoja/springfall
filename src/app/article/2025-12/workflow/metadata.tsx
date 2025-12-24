a82eb1363f8d0094a58d59f8ccf8bb5ba0c6e297
Kim Tae Hoon <eszqsc112@gmail.com>
2025-12-24 14:01:45 +0900

    workflow 경로 수정

--- src/app/article/2025-12/workflow/metadata.tsx
+++ src/app/article/2025-12/workflow/metadata.tsx
@@ -5,14 +5,14 @@ import i1 from "./w.jpg";
 
 export const title =
   "[TypeScript] 복구(롤백) 로직이 있는 경량 워크플로우 시스템 적용하기";
-const slug = "article/2025-08/workflow";
+const slug = "article/2025-12/workflow";
 export const summary =
   "일련의 작업들이 모두 다 잘 진행되어야 비로소 성공하는, 하지만 각 단계에서 실패를 적절히 처리해야 하는 워크플로우 시스템을 간단히 만들어봅시다";
 
-export const createdAt = dayjs("2025-08-18").toISOString();
-export const updatedAt = dayjs("2025-08-18").toISOString();
+export const createdAt = dayjs("2025-12-18").toISOString();
+export const updatedAt = dayjs("2025-12-18").toISOString();
 export const image = i1;
-export const imageAlt = "TODO:이미지수정";
+export const imageAlt = "Workflow Illustration";
 export const category: Category = "기술";
 
 export const item: ArticleItem = {
